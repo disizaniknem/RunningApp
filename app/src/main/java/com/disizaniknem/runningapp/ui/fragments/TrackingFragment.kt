d6f0092e8ac1725cfae3d93f773c6ef473633962
disizaniknem <disizaniknem@gmail.com>
2021-02-05 20:58:34 +0100

    AJout du tracking service

--- app/src/main/java/com/disizaniknem/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/disizaniknem/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,10 +1,13 @@
 package com.disizaniknem.runningapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.disizaniknem.runningapp.R
+import com.disizaniknem.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.disizaniknem.runningapp.services.TrackingService
 import com.disizaniknem.runningapp.ui.viewmodels.MainViewModel
 import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,10 +24,20 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         super.onViewCreated(view, savedInstanceState)
         mapView.onCreate(savedInstanceState)
 
+        btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+
         mapView.getMapAsync {
             map = it
         }
     }
+
+    private fun sendCommandToService(action: String) =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
 
     override fun onStart() {
         super.onStart()
