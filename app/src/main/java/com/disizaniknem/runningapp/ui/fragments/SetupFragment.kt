062f6981ba91dbfaa8cccfac526d5e58413f6114
disizaniknem <disizaniknem@gmail.com>
2021-02-05 19:57:50 +0100

    Ajout des fragments et de la navigation + bottom navigation bar

--- app/src/main/java/com/disizaniknem/runningapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/disizaniknem/runningapp/ui/fragments/SetupFragment.kt
@@ -1,7 +1,20 @@
 package com.disizaniknem.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.disizaniknem.runningapp.R
+import kotlinx.android.synthetic.main.fragment_setup.*
 
-class SetupFragment : Fragment(R.layout.fragment_settings) {
+class SetupFragment : Fragment(R.layout.fragment_setup) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
+    }
+
 }
