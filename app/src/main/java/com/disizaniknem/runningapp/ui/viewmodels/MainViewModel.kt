74d7090028590fb738ee00d3c6e4b8bebf0e8fa2
disizaniknem <disizaniknem@gmail.com>
2021-02-06 16:18:20 +0100

    Sauvegarde d'une course dans la base de données

--- app/src/main/java/com/disizaniknem/runningapp/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/disizaniknem/runningapp/ui/viewmodels/MainViewModel.kt
@@ -2,10 +2,17 @@ package com.disizaniknem.runningapp.ui.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.disizaniknem.runningapp.db.Run
 import com.disizaniknem.runningapp.repositories.MainRepository
+import kotlinx.coroutines.launch
 
 class MainViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ) : ViewModel() {
+
+    fun insertRun(run: Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 
 }
