96d56d9f3b12d54cdc38c4e78ad7da142428f2fe
disizaniknem <disizaniknem@gmail.com>
2021-02-06 14:40:45 +0100

    Ajout module pour le service + Mise à jour du timer dans la notification

--- app/src/main/java/com/disizaniknem/runningapp/di/AppModule.kt
+++ app/src/main/java/com/disizaniknem/runningapp/di/AppModule.kt
@@ -9,11 +9,13 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
 object AppModule {
 
+    @Singleton
     @Provides
     fun provideRunningDatabase(
         @ApplicationContext app: Context
@@ -23,6 +25,7 @@ object AppModule {
         RUNNING_DATABASE_NAME
     ).build()
 
+    @Singleton
     @Provides
     fun provideRunDao(db: RunningDatabase) = db.getRunDao()
 }
