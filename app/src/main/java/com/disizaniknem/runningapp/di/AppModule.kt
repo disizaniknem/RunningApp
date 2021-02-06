89b00dd72fe154fcebfbfae11c46dc6276cabfad
disizaniknem <disizaniknem@gmail.com>
2021-02-06 18:22:28 +0100

    Shared preferences, reste bug a corriger lorsqu'on quitte l'app

--- app/src/main/java/com/disizaniknem/runningapp/di/AppModule.kt
+++ app/src/main/java/com/disizaniknem/runningapp/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.disizaniknem.runningapp.di
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.room.Room
 import com.disizaniknem.runningapp.db.RunningDatabase
+import com.disizaniknem.runningapp.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.disizaniknem.runningapp.other.Constants.KEY_NAME
+import com.disizaniknem.runningapp.other.Constants.KEY_WEIGHT
 import com.disizaniknem.runningapp.other.Constants.RUNNING_DATABASE_NAME
+import com.disizaniknem.runningapp.other.Constants.SHARED_PREFERENCES_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -28,4 +34,23 @@ object AppModule {
     @Singleton
     @Provides
     fun provideRunDao(db: RunningDatabase) = db.getRunDao()
+
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(
+        @ApplicationContext app: Context
+    ) = app.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+
+    @Singleton
+    @Provides
+    fun provideName(sharedPref: SharedPreferences) = sharedPref.getString(KEY_NAME, "") ?: ""
+
+    @Singleton
+    @Provides
+    fun provideWeight(sharedPref: SharedPreferences) = sharedPref.getFloat(KEY_WEIGHT, 67f)
+
+    @Singleton
+    @Provides
+    fun provideFirstTimeToggle(sharedPref: SharedPreferences) = sharedPref.getBoolean(
+        KEY_FIRST_TIME_TOGGLE, true)
 }
