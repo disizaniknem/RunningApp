1ce79d34ba646cf4c3eb4307541e420a3b3f330f
disizaniknem <disizaniknem@gmail.com>
2021-02-07 15:34:48 +0100

    Correction de quelques bug sur le TrackingFragment

--- app/src/main/java/com/disizaniknem/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/disizaniknem/runningapp/ui/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+        bottomNavigationView.setOnNavigationItemReselectedListener { /* do anything */ }
 
         navHostFragment.findNavController()
             .addOnDestinationChangedListener {_, destination, _ ->
